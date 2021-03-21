9a7adff16f060d11989eacd5dd0ad8cc4edf69c7
Christian Fries <email@christian-fries.de>
2021-03-21 16:57:45 +0100

    Addex example for helpful NPE

---
+++ src/main/java/com/christianfries/teaching/java/HelpfulNullPointerExceptions.java
@@ -0,0 +1,34 @@
+package com.christianfries.teaching.java;
+
+/**
+ * NullPointerExceptions are more specific and report the
+ * the name of the reference that (unexpectedly) null.
+ * 
+ * @author Christian Fries
+ */
+public class HelpfulNullPointerExceptions {
+
+	public static void main(String[] args) {
+		
+		double[][] matrix1 = new double[10][10];
+
+		double[][] matrix2 = null;
+		
+		try {
+			matrix1[4][7] = 4.0;
+			matrix2[4][7] = 1.0;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		double[] vector = null;
+		try {
+			vector[5] = 4.0;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+}
+
+}
