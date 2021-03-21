eac12ce804601ec113c32bc9ef18eb261f6c5b1c
Christian Fries <email@christian-fries.de>
2021-03-21 16:57:29 +0100

    Added example for instanceOf pattern matching.

---
+++ src/main/java/com/christianfries/teaching/java/InstanceOf.java
@@ -0,0 +1,41 @@
+package com.christianfries.teaching.java;
+
+/**
+ * We may automatically specify the casted reference in an instanceof test block.
+ * 
+ * @author Christian Fries
+ */
+public class InstanceOf {
+
+	public static void main(String[] args) {
+
+		(new InstanceOf()).test();
+
+	}
+
+	private void test() {
+
+		Object json = """
+				"name" : "Fries",
+				"firstname" : "Christian",
+				"hobbies" : [
+					"computer",
+					"bike",
+					"movies",
+					"math",
+					"family"
+				]
+				""";
+
+		if(json instanceof String jsonAsString) {
+			String upper = jsonAsString.toUpperCase();
+			System.out.println(upper);
+		}
+		
+		if(json instanceof Number dataAsNumber) {
+			Double value = dataAsNumber.doubleValue() * 2.0;
+			System.out.println(value);
+		}
+	}
+
+}
